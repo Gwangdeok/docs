a98119133a18ea666e962a0bd2dabd88019a2eea
SID12g <sid12g@naver.com>
2025-06-10 09:54:48 +0900

    feat: add default page

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,5 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 /**
  * Shared layout configurations
  *
@@ -10,17 +10,10 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
+      <span className="inline-flex text-[15px] items-center font-medium gap-1">
+        <Image src="/logo.png" alt="광덕방범대" width={24} height={24} />
+        광덕방범대
+      </span>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
